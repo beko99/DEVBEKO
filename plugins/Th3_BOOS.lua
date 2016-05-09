4cd00d864026484f51ef58af92dc47a89085ae6d
Th3_BOOS <uop3uop@gmail.com>
2016-05-09 23:29:44 +0300

    Add files via upload

---
+++ plugins/Th3_BOOS.lua
@@ -0,0 +1,25 @@
+﻿do 
+
+local function run(msg, matches) 
+
+if ( msg.text ) then
+
+  if ( msg.to.type == "user" ) then
+
+     return "للتحدث مع المطور اضغط على المعرف التالي \n @Th3_BOOS \n  👾 "
+     
+  end 
+   
+end 
+
+-- #DEV @Th3_BOOS 
+
+return { 
+  patterns = { 
+       "(.*)$"
+  }, 
+  run = run, 
+} 
+
+end 
+-- By @Th3_BOOS
---
+++ plugins/Th3_BOOS1.lua
@@ -0,0 +1,24 @@
+--[[ 
+▀▄ ▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀▄▀▄▄▀▀▄▄▀▀▄▄▀▀▄▄▀▀          
+▀▄ ▄▀                                      ▀▄ ▄▀ 
+▀▄ ▄▀    BY SAJJAD NOORI                   ▀▄ ▄▀ 
+▀▄ ▄▀     BY SAJAD NOORI (@SAJJADNOORI)    ▀▄ ▄▀ 
+▀▄ ▄▀ JUST WRITED BY SAJJAD NOORI          ▀▄ ▄▀   
+▀▄ ▄▀          Dev  : المطور               ▀▄ ▄▀ 
+▀▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀▄▄▀▀▄▄▀▄▄▀▀
+--]]
+
+do
+
+function run(msg, matches)
+  return '❣ https://github.com/SAJJAD94/ASD_KARBALA.git \n❣   #المطور :@Th3_BOOS \n'
+end
+
+return {
+  patterns = {
+    "^اصدار"
+  }, 
+  run = run 
+}
+
+end
